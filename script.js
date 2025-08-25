f17b4edae8f80daced49d025bf605d58b8ed1a8e
IzamAhmed786 <izamahmed786@gmail.com>
2025-08-25 09:36:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let price = document.querySelectorAll(".price");
+  let total = 0;
+
+	for (let t of price) {
+		total=total+price
+		
+	}
   
 };
 
